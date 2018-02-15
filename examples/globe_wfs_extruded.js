c273d6f751fbc7494377a91663178d8a3b040cf3
Gerald Choqueux <gerald.choqueux@ign.fr>
2018-02-15 13:10:23 +0100

    example add pick in wfs add id buffer in feature to mesh

--- examples/globe_wfs_extruded.js
+++ examples/globe_wfs_extruded.js
@@ -33,65 +33,65 @@ promises.push(itowns.Fetcher.json('./layers/JSONLayers/Ortho.json').then(addLaye
 promises.push(itowns.Fetcher.json('./layers/JSONLayers/WORLD_DTM.json').then(addLayerCb));
 promises.push(itowns.Fetcher.json('./layers/JSONLayers/IGN_MNT_HIGHRES.json').then(addLayerCb));
 
-function altitudeLine(properties, contour) {
-    var altitudes = [];
-    var i = 0;
-    var result;
-    var tile;
-    var layer = globeView.wgs84TileLayer;
-    if (contour.length && contour.length > 0) {
-        for (; i < contour.length; i++) {
-            result = itowns.DEMUtils.getElevationValueAt(layer, contour[i], 0, tile);
-            if (!result) {
-                result = itowns.DEMUtils.getElevationValueAt(layer, contour[i], 0);
-            }
-            tile = [result.tile];
-            altitudes.push(result.z + 2);
-        }
-        return altitudes;
-    }
-    return 0;
-}
+// function altitudeLine(properties, contour) {
+//     var altitudes = [];
+//     var i = 0;
+//     var result;
+//     var tile;
+//     var layer = globeView.wgs84TileLayer;
+//     if (contour.length && contour.length > 0) {
+//         for (; i < contour.length; i++) {
+//             result = itowns.DEMUtils.getElevationValueAt(layer, contour[i], 0, tile);
+//             if (!result) {
+//                 result = itowns.DEMUtils.getElevationValueAt(layer, contour[i], 0);
+//             }
+//             tile = [result.tile];
+//             altitudes.push(result.z + 2);
+//         }
+//         return altitudes;
+//     }
+//     return 0;
+// }
 
-function colorLine(properties) {
-    var rgb = properties.couleur.split(' ');
-    return new itowns.THREE.Color(rgb[0] / 255, rgb[1] / 255, rgb[2] / 255);
-}
+// function colorLine(properties) {
+//     var rgb = properties.couleur.split(' ');
+//     return new itowns.THREE.Color(rgb[0] / 255, rgb[1] / 255, rgb[2] / 255);
+// }
 
-function acceptFeatureBus(properties) {
-    var line = properties.ligne + properties.sens;
-    if (linesBus.indexOf(line) === -1) {
-        linesBus.push(line);
-        return true;
-    }
-    return false;
-}
+// function acceptFeatureBus(properties) {
+//     var line = properties.ligne + properties.sens;
+//     if (linesBus.indexOf(line) === -1) {
+//         linesBus.push(line);
+//         return true;
+//     }
+//     return false;
+// }
 
-globeView.addLayer({
-    type: 'geometry',
-    update: itowns.FeatureProcessing.update,
-    convert: itowns.Feature2Mesh.convert({
-        color: colorLine,
-        altitude: altitudeLine }),
-    linewidth: 5,
-    filter: acceptFeatureBus,
-    url: 'https://download.data.grandlyon.com/wfs/rdata?',
-    protocol: 'wfs',
-    version: '2.0.0',
-    id: 'WFS Bus lines',
-    typeName: 'tcl_sytral.tcllignebus',
-    level: 9,
-    projection: 'EPSG:3946',
-    extent: {
-        west: 1822174.60,
-        east: 1868247.07,
-        south: 5138876.75,
-        north: 5205890.19,
-    },
-    options: {
-        mimetype: 'geojson',
-    },
-}, globeView.tileLayer);
+// globeView.addLayer({
+//     type: 'geometry',
+//     update: itowns.FeatureProcessing.update,
+//     convert: itowns.Feature2Mesh.convert({
+//         color: colorLine,
+//         altitude: altitudeLine }),
+//     linewidth: 5,
+//     filter: acceptFeatureBus,
+//     url: 'https://download.data.grandlyon.com/wfs/rdata?',
+//     protocol: 'wfs',
+//     version: '2.0.0',
+//     id: 'WFS Bus lines',
+//     typeName: 'tcl_sytral.tcllignebus',
+//     level: 9,
+//     projection: 'EPSG:3946',
+//     extent: {
+//         west: 1822174.60,
+//         east: 1868247.07,
+//         south: 5138876.75,
+//         north: 5205890.19,
+//     },
+//     options: {
+//         mimetype: 'geojson',
+//     },
+// }, globeView.tileLayer);
 
 function colorBuildings(properties) {
     if (properties.id.indexOf('bati_remarquable') === 0) {
@@ -114,24 +114,24 @@ function acceptFeature(properties) {
     return !!properties.hauteur;
 }
 
-scaler = function update(/* dt */) {
-    var i;
-    var mesh;
-    if (meshes.length) {
-        globeView.notifyChange(true);
-    }
-    for (i = 0; i < meshes.length; i++) {
-        mesh = meshes[i];
-        if (mesh.children.length) {
-            mesh.scale.z = Math.min(
-                1.0, mesh.scale.z + 0.016);
-            mesh.updateMatrixWorld(true);
-        }
-    }
-    meshes = meshes.filter(function filter(m) { return m.scale.z < 1; });
-};
+// scaler = function update(/* dt */) {
+//     var i;
+//     var mesh;
+//     if (meshes.length) {
+//         globeView.notifyChange(true);
+//     }
+//     for (i = 0; i < meshes.length; i++) {
+//         mesh = meshes[i];
+//         if (mesh.children.length) {
+//             mesh.scale.z = Math.min(
+//                 1.0, mesh.scale.z + 0.016);
+//             mesh.updateMatrixWorld(true);
+//         }
+//     }
+//     meshes = meshes.filter(function filter(m) { return m.scale.z < 1; });
+// };
 
-globeView.addFrameRequester(itowns.MAIN_LOOP_EVENTS.BEFORE_RENDER, scaler);
+// globeView.addFrameRequester(itowns.MAIN_LOOP_EVENTS.BEFORE_RENDER, scaler);
 globeView.addLayer({
     type: 'geometry',
     update: itowns.FeatureProcessing.update,
@@ -139,10 +139,10 @@ globeView.addLayer({
         color: colorBuildings,
         altitude: altitudeBuildings,
         extrude: extrudeBuildings }),
-    onMeshCreated: function scaleZ(mesh) {
-        mesh.scale.z = 0.01;
-        meshes.push(mesh);
-    },
+    // onMeshCreated: function scaleZ(mesh) {
+    //     mesh.scale.z = 0.01;
+    //     meshes.push(mesh);
+    // },
     filter: acceptFeature,
     url: 'http://wxs.ign.fr/72hpsel8j8nhb5qgdh07gcyp/geoportail/wfs?',
     networkOptions: { crossOrigin: 'anonymous' },
@@ -158,53 +158,53 @@ globeView.addLayer({
     },
 }, globeView.tileLayer);
 
-function configPointMaterial(result) {
-    var i = 0;
-    var mesh;
-    for (; i < result.children.length; i++) {
-        mesh = result.children[i];
+// function configPointMaterial(result) {
+//     var i = 0;
+//     var mesh;
+//     for (; i < result.children.length; i++) {
+//         mesh = result.children[i];
 
-        mesh.material.sizeAttenuation = false;
-    }
-}
+//         mesh.material.sizeAttenuation = false;
+//     }
+// }
 
-function colorPoint(/* properties */) {
-    return new itowns.THREE.Color(0x7F180D);
-}
+// function colorPoint(/* properties */) {
+//     return new itowns.THREE.Color(0x7F180D);
+// }
 
-function selectRoad(properties) {
-    return properties.gestion === 'CEREMA';
-}
+// function selectRoad(properties) {
+//     return properties.gestion === 'CEREMA';
+// }
 
-function altitudePoint(properties, contour) {
-    if (contour.length && contour.length > 0) {
-        return itowns.DEMUtils.getElevationValueAt(globeView.wgs84TileLayer, contour[0]).z + 5;
-    }
-    return 0;
-}
+// function altitudePoint(properties, contour) {
+//     if (contour.length && contour.length > 0) {
+//         return itowns.DEMUtils.getElevationValueAt(globeView.wgs84TileLayer, contour[0]).z + 5;
+//     }
+//     return 0;
+// }
 
-globeView.addLayer({
-    type: 'geometry',
-    update: itowns.FeatureProcessing.update,
-    convert: itowns.Feature2Mesh.convert({
-        altitude: altitudePoint,
-        color: colorPoint }),
-    size: 5,
-    onMeshCreated: configPointMaterial,
-    filter: selectRoad,
-    url: 'http://wxs.ign.fr/72hpsel8j8nhb5qgdh07gcyp/geoportail/wfs?',
-    networkOptions: { crossOrigin: 'anonymous' },
-    protocol: 'wfs',
-    version: '2.0.0',
-    id: 'WFS Route points',
-    typeName: 'BDPR_BDD_FXX_LAMB93_20170911:pr',
-    level: 12,
-    projection: 'EPSG:2154',
-    ipr: 'IGN',
-    options: {
-        mimetype: 'json',
-    },
-}, globeView.tileLayer);
+// globeView.addLayer({
+//     type: 'geometry',
+//     update: itowns.FeatureProcessing.update,
+//     convert: itowns.Feature2Mesh.convert({
+//         altitude: altitudePoint,
+//         color: colorPoint }),
+//     size: 5,
+//     onMeshCreated: configPointMaterial,
+//     filter: selectRoad,
+//     url: 'http://wxs.ign.fr/72hpsel8j8nhb5qgdh07gcyp/geoportail/wfs?',
+//     networkOptions: { crossOrigin: 'anonymous' },
+//     protocol: 'wfs',
+//     version: '2.0.0',
+//     id: 'WFS Route points',
+//     typeName: 'BDPR_BDD_FXX_LAMB93_20170911:pr',
+//     level: 12,
+//     projection: 'EPSG:2154',
+//     ipr: 'IGN',
+//     options: {
+//         mimetype: 'json',
+//     },
+// }, globeView.tileLayer);
 
 exports.view = globeView;
 exports.initialPosition = positionOnGlobe;
--- src/Renderer/ThreeExtended/Feature2Mesh.js
+++ src/Renderer/ThreeExtended/Feature2Mesh.js
@@ -242,6 +242,7 @@ function coordinateToPolygonExtruded(coordinates, properties, options) {
     const indices = [];
     const vertices = new Float32Array(2 * 3 * coordinates.coordinates.length);
     const colors = new Uint8Array(3 * 2 * coordinates.coordinates.length);
+    const ids = new Uint16Array(2 * coordinates.coordinates.length);
     const geometry = new THREE.BufferGeometry();
     let offset = 0;
     let offset2 = 0;
@@ -266,6 +267,10 @@ function coordinateToPolygonExtruded(coordinates, properties, options) {
         for (const indice of triangles) {
             indices.push(offset + indice);
         }
+        const ofid = Math.floor(offset2 / 3);
+        for (let i = ofid; i < contour.length + ofid; ++i) {
+            ids[i] = property._idx;
+        }
         offset2 += nbVertices * 2;
         addFaces(indices, contour.length, offset);
         // assign color to each point
@@ -277,6 +282,7 @@ function coordinateToPolygonExtruded(coordinates, properties, options) {
     }
     geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
     geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3, true));
+    geometry.addAttribute('id', new THREE.BufferAttribute(ids, 1));
     geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(indices), 1));
     const result = new THREE.Mesh(geometry);
     result.minAltitude = minAltitude;
